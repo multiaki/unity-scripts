ec2ad1cfe35f27262146501bf26e5d70971ad510
Aki Sharipov <aki@multiaki.com>
2015-07-03 14:44:29 -0400

    added keyboard control

---
+++ MoveObjectWithKey.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+
+public var speed : float = 10f;
+
+
+function Update () {
+	
+	var move = Vector3(Input.GetAxis('Horizontal'), Input.GetAxis('Vertical'), 0);
+	transform.position += move * speed * Time.deltaTime;
+	
+}
