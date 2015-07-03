bde6369864ad4616b206392708996b290c1bd32f
Aki Sharipov <aki@multiaki.com>
2015-07-03 12:23:19 -0400

    changed some scripts

---
+++ MoveObject.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+public var xSpeed: int;
+public var ySpeed: int;
+
+public var randomX : boolean;
+public var randomY : boolean;
+
+public var minXSpeed : int = 100;
+public var maxXSpeed : int = 200;
+
+public var minYSpeed : int = 100;
+public var maxYSpeed : int = 200;
+
+public var randomScaleMin : float = 0.5f;
+public var randomScaleMax : float = 2f;
+
+function Start () {
+
+	if(randomX) 
+		xSpeed = Random.Range(minXSpeed, maxXSpeed);
+		
+	if(randomY) 
+		ySpeed = Random.Range(minYSpeed, maxYSpeed);
+
+	var newSpeed : Vector2;
+	newSpeed.x = xSpeed;
+	newSpeed.y = ySpeed;
+	
+	var newScale : Vector3;
+	var scale = Random.Range(randomScaleMin, randomScaleMax);
+	newScale.x = scale;
+	newScale.y = scale;
+	newScale.z = 0;
+
+	transform.localScale = newScale;
+//	GetComponent.<Transform>().localScale(newScale);
+	
+	GetComponent.<Rigidbody2D>().AddForce(newSpeed);
+}
--- instantiateGObj.js
+++ instantiateGObj.js
@@ -2,16 +2,28 @@
 
 public var gObj: GameObject;
 
+public var MinX:float = 0f;
+public var MaxX:float = 0f;
+
+public var MinY:float = 0f;
+public var MaxY:float = 0f;
+
+public var StartDelay : float = 0;
+public var InstantiateDelay: float = 0;
+
+
+
+
 function Start () {
-	InvokeRepeating('createRandom', 1, 2);
+	InvokeRepeating('createRandom', StartDelay, InstantiateDelay);
 }
 
 function createRandom () {
 	var x : float;
 	var y : float;
 	
-	x = Random.Range(-6.0, 6.0);
-	y = 6;
+	x = Random.Range(MinX, MaxX);
+	y = Random.Range(MinY, MaxY);
 	
 	
 	Instantiate(gObj, Vector3(x, y, 0), Quaternion.identity);
